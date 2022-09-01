065bf4acd67fd7370661193591604d0fbaa28652
matth1eumartin <94123711+matth1eumartin@users.noreply.github.com>
2022-09-01 16:23:37 +0100

    Update App.js

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -36,6 +36,7 @@ function App() {
       towards an MInf in Informatics from which I will be graduating in 2026.
       <br></br>I am a mostly back-end focused Engineer, with a strong grasp on Python and a good knowledge of a whole bunch of Data Structures and Algorithms. I'm also fascinated by machine learning and artificial intelligence which has made me familiarise myself with many common Data Science packages such as Pandas, Scikit and TensorFlow. I also made this site from scratch using ReactJS, just to try and pick something up on the side - I buy strongly into the idea that a programmer needs to be extremely multi-faceted and constantly hungry to learn more. </p>
       <p>On a more personal note, when I'm not working, I'm interested in road cycling and football, but I also enjoy a good book - my fluency in French as well as English means I am blessed with a large pool to choose from. </p>
+      <p>Wait did this work</p>
     </div>
   );
 }
