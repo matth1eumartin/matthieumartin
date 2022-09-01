956574741fec6b8f4c3dd5fd08c3d4cdcd9349a9
matth1eumartin <94123711+matth1eumartin@users.noreply.github.com>
2022-09-01 18:51:23 +0100

    description update + icon
    Rewrote the description + updated the icon for webpage

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -35,10 +35,9 @@ function App() {
       <p id = 'intro'>Hi, my name is <span id='Name'>Matthieu</span></p>
       <p id = 'scrolling-text'>I am <span ref = {el}></span></p>
       <br></br>
-      <p id = 'description'>I'm currently a second year student at the University of Edinburgh studying 
-      towards an MInf in Informatics from which I will be graduating in 2026.
-      <br></br>I am a mostly back-end focused Engineer, with a strong grasp on Python and a good knowledge of a whole bunch of Data Structures and Algorithms. I'm also fascinated by machine learning and artificial intelligence which has made me familiarise myself with many common Data Science packages such as Pandas, Scikit and TensorFlow. I also made this site from scratch using ReactJS, just to try and pick something up on the side - I buy strongly into the idea that a programmer needs to be extremely multi-faceted and constantly hungry to learn more. </p>
-      <p>On a more personal note, when I'm not working, I'm interested in road cycling and football, but I also enjoy a good book - my fluency in French as well as English means I am blessed with a large pool to choose from. </p>
+      <p id = 'description'>I'm currently just about to go into my second year as a student at the University of Edinburgh. I'm enrolled in the 5 year integrated masters informatics course (MInf), and will graduate in 2026'.
+      <br></br>I am a mostly back-end focused Engineer, with a strong grasp on Python and a good knowledge of a whole bunch of Data Structures and Algorithms. I've also developed a good level in Java and Haskell thanks to University. I'm also fascinated by machine learning and artificial intelligence which has made me familiarise myself with many common Data Science packages such as Pandas, Scikit and TensorFlow. I also made this site from scratch using ReactJS, just to try and pick something up on the side - I buy strongly into the idea that a programmer needs to be extremely multi-faceted and constantly hungry to learn more. </p>
+      
       
       <a href = "https://github.com/matth1eumartin">
       <img src={githubNormallogo} alt='github link' 
