23bda82529267b91dd488af5381860bed727b301
matth1eumartin <94123711+matth1eumartin@users.noreply.github.com>
2022-09-03 19:07:37 +0100

    Small change
    Changed an I am to I'm

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -46,7 +46,7 @@ function App() {
       <p id = 'typing-text'>I am <span ref = {el}></span></p>
       <br></br>
       <p id = 'description'>I'm currently just about to go into my second year as a student at the University of Edinburgh. I'm enrolled in the 5 year integrated masters Informatics course (MInf), and will graduate in 2026.
-      I am a mostly back-end focused Engineer, with a strong grasp on Python and a good knowledge of a whole bunch of Data Structures and Algorithms. I've also developed a good level in Java and Haskell thanks to University. I'm also fascinated by machine learning and artificial intelligence which has made me familiarise myself with many common Data Science packages such as Pandas, Scikit and TensorFlow. I also made this site from scratch using ReactJS, just to try and pick something up on the side - I buy strongly into the idea that a programmer needs to be extremely multi-faceted and constantly hungry to learn more. </p>
+      I'm a mostly back-end focused Engineer, with a strong grasp on Python and a good knowledge of a whole bunch of Data Structures and Algorithms. I've also developed a good level in Java and Haskell thanks to University. I'm also fascinated by machine learning and artificial intelligence which has made me familiarise myself with many common Data Science packages such as Pandas, Scikit and TensorFlow. I also made this site from scratch using ReactJS, just to try and pick something up on the side - I buy strongly into the idea that a programmer needs to be extremely multi-faceted and constantly hungry to learn more. </p>
       <p>The links to my github and linkedin and sending me an email lie below, just click on the respective logos!</p>
       
       <div class='images'>
