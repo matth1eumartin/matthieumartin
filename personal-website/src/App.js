d924164cabe7d9b141749b75a04913546c9b99d2
matth1eumartin <94123711+matth1eumartin@users.noreply.github.com>
2022-09-03 18:42:31 +0100

    Landpage Update
    Added a small doodle, the arrow and resized the intro text.

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -3,6 +3,10 @@ import githubNormallogo from './githubNormal.png'
 import githubMouseOverLogo from './gitHubMouseOver.png'
 import linkedinNormalLogo from './linkedinNormal.png'
 import linkedinMouseOver from './linkedinMouseOver.png'
+import gmail from './gmail.png'
+import gmailMouseOver from './gmailMouseOver.png'
+import me from './realme.png'
+import downarrow from './down-arrow.png'
 import Typed from "typed.js";
 import { useEffect, useRef } from "react";
 
@@ -14,12 +18,12 @@ function App() {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: [" a student", "an award winner", "a problem solver", "a self-starter", "a Software Engineer", "a team player", "a Data Scientist", "the person you want to hire :)"],
-      startDelay: 0,
-      typeSpeed: 40,
-      backSpeed: 40,
-      backDelay: 700,
-      smartBackspace: true,
+      strings: [" a student", "an award winner", "a problem solver", "a self-starter", "a Software Engineer", "a quick learner", "a team player", "a Data Scientist", "the person you want to hire :)"],
+      startDelay: 1000,
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 1500,
+      smartBackspace: false,
       cursorChar: '_',
       loop: true
     });
@@ -32,26 +36,42 @@ function App() {
 
   return (
     <div class="App">
-      <p id = 'intro'>Hi, my name is <span id='Name'>Matthieu</span></p>
-      <p id = 'scrolling-text'>I am <span ref = {el}></span></p>
+
+      <div class = "pageIntro">
+        <p id = 'intro'>Hi! My name is <br></br><span id='Name'>Matthieu</span></p>
+        <img src={me} id='stickMan' width ="200" height="200"></img>
+        <img src={downarrow} id='downarrow' width="30" height="40"></img>
+      </div>
+
+      <p id = 'typing-text'>I am <span ref = {el}></span></p>
       <br></br>
-      <p id = 'description'>I'm currently just about to go into my second year as a student at the University of Edinburgh. I'm enrolled in the 5 year integrated masters informatics course (MInf), and will graduate in 2026'.
-      <br></br>I am a mostly back-end focused Engineer, with a strong grasp on Python and a good knowledge of a whole bunch of Data Structures and Algorithms. I've also developed a good level in Java and Haskell thanks to University. I'm also fascinated by machine learning and artificial intelligence which has made me familiarise myself with many common Data Science packages such as Pandas, Scikit and TensorFlow. I also made this site from scratch using ReactJS, just to try and pick something up on the side - I buy strongly into the idea that a programmer needs to be extremely multi-faceted and constantly hungry to learn more. </p>
-      <p>The links to my github and linkedin lie below, just click on the respective logos!</p>
+      <p id = 'description'>I'm currently just about to go into my second year as a student at the University of Edinburgh. I'm enrolled in the 5 year integrated masters Informatics course (MInf), and will graduate in 2026.
+      I am a mostly back-end focused Engineer, with a strong grasp on Python and a good knowledge of a whole bunch of Data Structures and Algorithms. I've also developed a good level in Java and Haskell thanks to University. I'm also fascinated by machine learning and artificial intelligence which has made me familiarise myself with many common Data Science packages such as Pandas, Scikit and TensorFlow. I also made this site from scratch using ReactJS, just to try and pick something up on the side - I buy strongly into the idea that a programmer needs to be extremely multi-faceted and constantly hungry to learn more. </p>
+      <p>The links to my github and linkedin and sending me an email lie below, just click on the respective logos!</p>
       
+      <div class='images'>
       <a href = "https://github.com/matth1eumartin">
-      <img src={githubNormallogo} alt='github link' 
+      <img src={githubNormallogo} width="50px" height="50px" alt='github link' 
       onMouseOver={e => e.currentTarget.src=githubMouseOverLogo}
-      onMouseOut= {e => e.currentTarget.src=githubNormallogo}
+      onMouseOut={e => e.currentTarget.src=githubNormallogo}
       id = 'githubLogo'></img>
       </a>
 
       <a href = "https://www.linkedin.com/in/matthieu-martin-/">
-        <img src={linkedinNormalLogo} alt='linkedin link' 
+        <img src={linkedinNormalLogo} width="50px" height="50px" alt='linkedin link' 
         onMouseOver={e => e.currentTarget.src=linkedinMouseOver}
-        onMouseOut= {e => e.currentTarget.src=linkedinNormalLogo}
+        onMouseOut={e => e.currentTarget.src=linkedinNormalLogo}
         id = 'linkedinLogo'></img>
       </a>
+
+      <a href = "mailto:matthieum2003@gmail.com">
+        <img src={gmail} width="50px" height="50px" alt='mailto gmail link' 
+        onMouseOver={e => e.currentTarget.src=gmailMouseOver}
+        onMouseOut={e => e.currentTarget.src=gmail}
+        id = 'linkedinLogo'></img>
+      </a>
+      </div>
+
     </div>
   );
 }
