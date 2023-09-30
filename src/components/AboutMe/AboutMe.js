3c730228a023a39c11acf5ec84752fa12abf6e73
matth1eumartin <94123711+matth1eumartin@users.noreply.github.com>
2023-09-30 12:13:09 +0100

    Update for 3rd year :)

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const AboutMe = () => {
   return (
-    <div className='px-40'>
-        <h1 className="text-6xl font-bold pb-12">about me</h1>
-        <p className="text-2xl">I'm currently in my second year out of five at the University of Edinburgh, studying towards a Master of Informatics.</p>
+    <div className="px-40">
+      <h1 className="text-6xl font-bold pb-12">about me</h1>
+      <p className="text-2xl">
+        I'm currently in my third year at the University of Edinburgh, studying
+        towards a Master of Informatics. 
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ProjectLeftSide from './ProjectLeftSide';
-import ProjectRightSide from './ProjectRightSide';
+import React from "react";
+import ProjectLeftSide from "./ProjectLeftSide";
+import ProjectRightSide from "./ProjectRightSide";
 
 const ProjectList = () => {
   return (
-    <div className='px-40 pt-20'>
-        <h1 className='text-6xl font-bold pb-12'>projects</h1>
-        <div className='pb-16'>
-          <ProjectLeftSide 
+    <div className="px-40 pt-20">
+      <h1 className="text-6xl font-bold pb-12">projects</h1>
+      <div className="pb-16">
+        <ProjectLeftSide
+          title="Automated TikTok Video Generator"
+          techStack="python, pygame, numpy, librosa, beautifulsoup4, requests, moviepy"
+          description="A program which generates TikTok videos of visualisations of particles reacting to music features in trending TikTok audios."
+          primaryImage="https://github.com/matth1eumartin/matthieumartin/assets/94123711/b39c6ce1-c609-4671-ad39-9e5e5620d822"
+          primaryImageAlt="A sample image of the output of the program."
+          secondaryImage="https://github.com/matth1eumartin/matthieumartin/assets/94123711/79d13f96-6780-4c54-ae6b-64d7d87d169b"
+          secondaryImageAlt="Another sample image of the output of the program."
+          projectLink="https://www.tiktok.com/@trendingsoundsvisualised"
+        />
+        <ProjectRightSide
+          title="Globe for Spotify"
+          techStack="javascript, react, tailwindcss, spotify api"
+          description="A website which allows users to visualise their Spotify listening history on a 3 dimensional interactive globe, seeing where on the planet their favourite songs are listened to. Currently deployed on Vercel."
+          primaryImage="https://github.com/matth1eumartin/matthieumartin/assets/94123711/3aa31117-7594-4ee5-9064-98b69c88d4e3"
+          primaryImageAlt="A sample image of interaction with the website."
+          secondaryImage="https://github.com/matth1eumartin/matthieumartin/assets/94123711/d8291183-be70-409a-b9ef-a86e82c1e714"
+          secondaryImageAlt="A sample image of the output of the app."
+          projectLink="https://globeforspotify.vercel.app"
+        />
+        <ProjectLeftSide
           title="Pathfinding Visualisation"
           techStack="python, pygame"
           description="I created a visualisation of the A* and Dijkstra pathfinding algorithms using pygame."
@@ -16,8 +36,8 @@ const ProjectList = () => {
           secondaryImage="https://user-images.githubusercontent.com/94123711/189365987-f01689d5-0634-4c28-b8c8-79539b2d6fd0.gif"
           secondaryImageAlt="A short gif of the Dijkstra algorithm operating on a grid of nodes."
           projectLink="https://github.com/matth1eumartin/Pathfinding-Visualisation"
-          />
-          <ProjectRightSide
+        />
+        <ProjectRightSide
           title="Premier League Elo Analysis"
           techStack="python, matplotlib, pandas"
           description="Using a dataset which contained all the results in the English Men's premier league since 1992/32, I created an ELO system to rank the teams and identify the best teams of all time."
@@ -26,8 +46,8 @@ const ProjectList = () => {
           secondaryImage="https://user-images.githubusercontent.com/94123711/188881110-86c679db-7dc3-432c-b5e2-0288e6cdf40d.png"
           secondaryImageAlt="A graph depicting the output of the ELO system on Manchester United F.C. fixtures."
           projectLink="https://github.com/matth1eumartin/PremierLeagueElos"
-          />
-          <ProjectLeftSide 
+        />
+        <ProjectLeftSide
           title="Haskell Art Competition Entry"
           techStack="haskell, gloss"
           description="My entry to the INF1A Haskell Art Competition held at the end of my first semester, which won 5th place."
@@ -36,20 +56,10 @@ const ProjectList = () => {
           secondaryImage=""
           secondaryImageAlt=""
           projectLink="https://github.com/matth1eumartin/Inf1A-FP-Comp"
-          />
-          <ProjectRightSide
-          title="Statify"
-          techStack="javascript, react.js, axios"
-          description="A web application using react.js that uses the Spotify API to generate the top 10 songs that a use has listened to over the path month."
-          primaryImage="https://user-images.githubusercontent.com/94123711/193327670-b5249ff5-38b4-4c75-8c20-ea56028959f2.png"
-          primaryImageAlt="An image of the Statify homepage."
-          secondaryImage="https://user-images.githubusercontent.com/94123711/193328201-3f2057ae-6760-416e-9299-2390e09904af.png"
-          secondaryImageAlt="An example output of the Statify results."
-          projectLink="https://matth1eumartin.github.io/statify/"
-          />
-        </div>
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectList
+export default ProjectList;
